a740b04c04f654eea67d99c3593ae02fbb530899
Jacob <jacobt.hayes64@gmail.com>
2023-03-01 18:54:28 -0500

    navbar components

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-import Navbar from "./components/navbar.component"
-import AppointmentList from  "./components/appointment-list.component"
-import EditAppointment from  "./components/edit-appointment.component"
-import CreateAppointment from  "./components/create-appointment.component"
-import CreateUser from  "./components/create-user.component"
+import Navbar from "./components/navbar.component.js"
+import AppointmentList from  "./components/appointment-list.component.js"
+import EditAppointment from  "./components/edit-appointment.component.js"
+import CreateAppointment from  "./components/create-appointment.component.js"
+import CreateUser from  "./components/create-user.component.js"
 
 function App() {
   return (
     <Router>
       <Navbar />
-      
-      <Route path="/" exact component={AppointmentList} />
+        <br/>
+      <Route path="/" exact element={AppointmentList} />
       <Route path="/edit/:id" component={EditAppointment} />
       <Route path="/create" component={CreateAppointment} />
       <Route path="/user" component={CreateUser} />
     </Router>
+
   );
 }
 export default App;
